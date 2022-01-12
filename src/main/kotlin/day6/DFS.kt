13f9700858ae783965b2e65c38984958e214e824
Bui Duc Lam <buiduclam09@gmail.com>
2022-01-12 20:20:30 +0700

    add dijkstra

--- src/main/kotlin/day6/DFS.kt
+++ src/main/kotlin/day6/DFS.kt
@@ -1,2 +1,11 @@
 package day6
 
+import sun.security.provider.certpath.Vertex
+
+fun main() {
+
+}
+
+fun depthFirstSearch(source: List<Int>): ArrayList<Int> {
+    return arrayListOf()
+}
---
+++ src/main/kotlin/day6/Graph.kt
@@ -0,0 +1,6 @@
+package day6
+
+class Graph<T> {
+
+
+}
